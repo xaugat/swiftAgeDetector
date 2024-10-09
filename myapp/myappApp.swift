251f467462143a829b6b46e84b2fd0a332f08e85
xaugat <sauugat@gmail.com>
2024-10-09 09:00:23 +0545

    Initial Commit

---
+++ myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by saugat poudel on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
